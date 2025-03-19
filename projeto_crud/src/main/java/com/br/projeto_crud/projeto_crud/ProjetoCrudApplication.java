e566d8c00652320fd1a9b19f11e015881835d849
Kelwin Gabriel <kelwimgabriel@gmail.com>
2025-03-19 19:00:53 -0300

    Criação da estrutura do projeto e arquivo README

---
+++ projeto_crud/src/main/java/com/br/projeto_crud/projeto_crud/ProjetoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.br.projeto_crud.projeto_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCrudApplication.class, args);
+	}
+
+}
---
+++ projeto_crud/src/test/java/com/br/projeto_crud/projeto_crud/ProjetoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projeto_crud.projeto_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
