cd8a9428013d63f45050ca224b230367500cef87
Natalyanjos <Nataly.anjos@aluno.ce.gov.br>
2025-04-08 17:48:15 -0300

    Criação da classe produto modelo

--- projeto_crud/src/main/java/com/br/projeto_crud/ProjetoCrudApplication.java
+++ projeto_crud/src/main/java/com/br/projeto_crud/ProjetoCrudApplication.java
@@ -1,4 +1,4 @@
-package com.br.projeto_crud.projeto_crud;
+package com.br.projeto_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
