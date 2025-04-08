06dc820ce2502d52263f0b1d2c0e4184ef303690
Natalyanjos <Nataly.anjos@aluno.ce.gov.br>
2025-04-08 17:58:39 -0300

    CORS resolvido!

--- projeto_crud/src/main/java/com/br/projeto_crud/controle/ProdutoControle.java
+++ projeto_crud/src/main/java/com/br/projeto_crud/controle/ProdutoControle.java
@@ -1,17 +1,21 @@
 package com.br.projeto_crud.controle;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.br.projeto_crud.modelo.ProdutoModelo;
 import com.br.projeto_crud.modelo.RespostaModelo;
 import com.br.projeto_crud.servico.ProdutoServico;
 
+@RestController
+@CrossOrigin(origins = "*")
 public class ProdutoControle {
 
   private ProdutoServico ps;
