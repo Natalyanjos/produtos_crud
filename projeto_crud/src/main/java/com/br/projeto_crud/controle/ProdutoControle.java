e057a3f2b8a1e6de7962a2c90544f357ad6b62fa
Natalyanjos <Nataly.anjos@aluno.ce.gov.br>
2025-04-08 17:57:21 -0300

    Criação da rota cadastrar

--- projeto_crud/src/main/java/com/br/projeto_crud/controle/ProdutoControle.java
+++ projeto_crud/src/main/java/com/br/projeto_crud/controle/ProdutoControle.java
@@ -1,6 +1,10 @@
 package com.br.projeto_crud.controle;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.br.projeto_crud.modelo.ProdutoModelo;
 import com.br.projeto_crud.servico.ProdutoServico;
@@ -8,6 +12,17 @@ import com.br.projeto_crud.servico.ProdutoServico;
 public class ProdutoControle {
 
   private ProdutoServico ps;
+
+  @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "cadastrar");
+    }
+    
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
+    
    @GetMapping("/listar")
     public Iterable<ProdutoModelo> listar(){
         return ps.listar();
--- projeto_crud/src/main/java/com/br/projeto_crud/servico/ProdutoServico.java
+++ projeto_crud/src/main/java/com/br/projeto_crud/servico/ProdutoServico.java
@@ -1,15 +1,37 @@
 package com.br.projeto_crud.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.br.projeto_crud.modelo.ProdutoModelo;
+import com.br.projeto_crud.modelo.RespostaModelo;
 import com.br.projeto_crud.repositorio.ProdutoRepositorio;
 
 public class ProdutoServico {
   @Autowired
-    private ProdutoRepositorio pr;
-    
-    public Iterable<ProdutoModelo> listar(){
-        return pr.findAll();
+  private ProdutoRepositorio pr;
+  @Autowired
+  private RespostaModelo rm;
+
+  public ResponseEntity <?> cadastrarAlterar(ProdutoModelo pm, String acao){
+        if(pm.getNome().equals("")){
+            rm.setResposta("O nome do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setResposta("A marca do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else{
+            if(acao.equals("cadastrar")){
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+            }else{
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.OK);
+                
+            }
+        }
     }
+
+  public Iterable<ProdutoModelo> listar() {
+    return pr.findAll();
+  }
 }
