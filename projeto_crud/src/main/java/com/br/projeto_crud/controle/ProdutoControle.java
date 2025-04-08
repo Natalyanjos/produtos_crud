0981c9cf35ad3f30679d77ce62fb56543aaa75d6
Natalyanjos <Nataly.anjos@aluno.ce.gov.br>
2025-04-08 17:55:01 -0300

    Criação do metodo listra() de serviço

--- projeto_crud/src/main/java/com/br/projeto_crud/controle/ProdutoControle.java
+++ projeto_crud/src/main/java/com/br/projeto_crud/controle/ProdutoControle.java
@@ -1,5 +1,20 @@
 package com.br.projeto_crud.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.br.projeto_crud.modelo.ProdutoModelo;
+import com.br.projeto_crud.servico.ProdutoServico;
+
 public class ProdutoControle {
-  
+
+  private ProdutoServico ps;
+   @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar(){
+        return ps.listar();
+    }
+
+    @GetMapping("/")
+    public String rota(){
+        return "A API está funcionando!";
+    }
 }
--- projeto_crud/src/main/java/com/br/projeto_crud/servico/ProdutoServico.java
+++ projeto_crud/src/main/java/com/br/projeto_crud/servico/ProdutoServico.java
@@ -1,5 +1,15 @@
 package com.br.projeto_crud.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.br.projeto_crud.modelo.ProdutoModelo;
+import com.br.projeto_crud.repositorio.ProdutoRepositorio;
+
 public class ProdutoServico {
-  
+  @Autowired
+    private ProdutoRepositorio pr;
+    
+    public Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
 }
